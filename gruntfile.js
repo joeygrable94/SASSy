09f4b4dc6edf59ba7b32457be05bef55a7c0a8a5
grablej919 <grablej919@gmail.com>
2018-08-01 09:36:24 -0700

    added bower components HTML5 reset and HTML5 boilerplate

--- gruntfile.js
+++ gruntfile.js
@@ -1,24 +1,18 @@
 // GRUNT
 module.exports = function(grunt) {
 
-	// directories
-	var theme = {
-		root: './wp-content/themes/NewReach'
-	};
+	// DIR
 	// development
 	var dev	= {
-		root: './dev_stack',
-		sass: '/sassy',
+		root: './src',
+		sass: '/sass',
 		css: '/css',
-		js: '/js',
 		vendors: '/vendors'
 	};
 	// production
 	var dist = {
-		root: theme.root + '/assets',
-		css: '/css',
-		images: '/images',
-		js: '/js'
+		root: './dist',
+		css: '/css'
 	};
 
 	// initial config
@@ -35,9 +29,9 @@ module.exports = function(grunt) {
 				},
 				files: [{
 					expand: true,
-					cwd: dev.root + dev.sass,
+					cwd: dev.root+dev.sass,
 					src: ['*.scss'],
-					dest: dev.root + dev.css,
+					dest: dev.root+dev.css,
 					ext: '.dev.css'
 				}]
 			},
@@ -47,82 +41,19 @@ module.exports = function(grunt) {
 				},
 				files: [{
 					expand: true,
-					cwd: dev.root + dev.sass,
+					cwd: dev.root+dev.sass,
 					src: ['*.scss'],
-					dest: dist.root + dist.css,
+					dest: dist.root+dist.css,
 					ext: '.min.css'
 				}]
-			}
-		},
-
-		// lint JS for errors
-		jshint: {
-			files: [
-				'gruntfile.js',
-				dev.root + dev.js + '/vendors/*.js',
-				dev.root + dev.js + '/src/*.js',
-				dev.root + dev.js + '/test/*.js',
-			],
-			options: {
-				esversion: 6,
-				globals: {
-					jQuery: true
-				}
-			}
-		},
-
-		// concat all js files
-		concat: {
-			options: {
-				separator: '\n\n\n\n\n/** ================================================== **/\n'
-			},
-			dist: {
-				src: [
-					dev.root + dev.js + '/vendors/*.js',
-					dev.root + dev.js + '/src/*.js',
-					dev.root + dev.js + '/test/*.js',
-				],
-				dest: dev.root + dev.js + '/concatenated/<%= pkg.name %>.js'
-			}
-		},
-
-		// convert JS ES6 to ES5 compatible
-		babel: {
-			options: {
-				sourceMap: true,
-				presets: ['env']
-			},
-			dist: {
-				files: [{
-					expand: true,
-					cwd: dev.root + dev.js + '/concatenated',
-					src: ['**/*.js'],
-					dest: dev.root + dev.js + '/compiled',
-					ext: '.compiled.js'
-				}]
-			}
-		},
-
-		// compress JS
-		uglify: {
-			options: {
-				mangle: true
-			},
-			build : {
-				src : [dev.root + dev.js + '/compiled/newreach.compiled.js'],
-				dest: dist.root + dist.js + 'newreach.min.js',
 			}
 		},
 
 		// watcher
 		watch: {
 			css: {
-				files: [dev.root + dev.sass + '/**/*.scss'],
-				tasks: ['sass']
-			},
-			js: {
-				files: [dev.root + dev.js + '/**/*.js'],
-				tasks: ['jshint', 'concat', 'babel']
+				files: [dev.root+dev.sass + '/**/*.scss'],
+				tasks: ['sass:dev']
 			}
 		}
 
@@ -130,15 +61,17 @@ module.exports = function(grunt) {
 
 	// load plugins
 	require('load-grunt-tasks')(grunt);
-	grunt.loadNpmTasks('grunt-babel');
 
 	// register tasks
 	grunt.registerTask('default', [
-		'sass',
-		'jshint',
-		'concat',
-		'babel',
-		'uglify'
+		'sass:dev'
 	]);
+
+	// package project for distribution
+	grunt.registerTask('package', [
+		'sass:dist'
+	]);
+
+
 
 };
